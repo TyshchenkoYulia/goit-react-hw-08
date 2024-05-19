eb4b79cc0cbc8981a0e7b7db6649aa4a8d816377
TyshchenkoYulia <145122847+TyshchenkoYulia@users.noreply.github.com>
2024-05-19 20:11:59 +0300

    create components

---
+++ src/components/App/App.jsx
@@ -0,0 +1,25 @@
+import { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import Layout from "../Layout/Layout";
+import Loader from "../Loader/Loader";
+import HomePage from "../../pages/HomePage/HomePage";
+import RegistrationPage from "../../pages/RegistrationPage/RegistrationPage";
+import LoginPage from "../../pages/LoginPage/LoginPage";
+import ContactsPage from "../../pages/ContactsPage/ContactsPage";
+
+export default function App() {
+  return (
+    <>
+      <Layout>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/register" element={<RegistrationPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/contacts" element={<ContactsPage />} />
+          </Routes>
+        </Suspense>
+      </Layout>
+    </>
+  );
+}
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,18 @@
+import css from "./Loader.module.css";
+import { Hearts } from "react-loader-spinner";
+
+export default function Loader() {
+  return (
+    <div className={css.container}>
+      <Hearts
+        height="50"
+        width="50"
+        color="rgb(81, 88, 212)"
+        ariaLabel="hearts-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+    </div>
+  );
+}
