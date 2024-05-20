65a329069fc4def3f9a2c2851d76994c0aef5378
TyshchenkoYulia <145122847+TyshchenkoYulia@users.noreply.github.com>
2024-05-20 14:32:22 +0300

    added registr form

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,8 +1,16 @@
 import { Formik, Form, Field } from "formik";
+import { useDispatch } from "react-redux";
+import { register } from "../../redux/auth/operations";
 import css from "./RegistrationForm.module.css";
 
 export default function RegistrationForm() {
-  const handleSubmit = () => {};
+  const dispatch = useDispatch();
+
+  const handleSubmit = (values, actions) => {
+    dispatch(register(values));
+    actions.resetForm();
+  };
+
   return (
     <Formik
       initialValues={{
--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,13 +1,28 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
+// test@gmail.com
+// test000@gmail.com
+
 axios.defaults.baseURL = "https://connections-api.herokuapp.com";
+
+const setAuthHeader = (token) => {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+};
+
+const clearAuthHeader = () => {
+  axios.defaults.headers.common["Authorization"] = "";
+};
 
 export const register = createAsyncThunk(
   "auth/register",
   async (newUser, thunkAPI) => {
     try {
       const response = await axios.post("/users/signup", newUser);
+
+      //   console.log(response.data);
+
+      setAuthHeader(response.data.token);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -20,6 +35,7 @@ export const logIn = createAsyncThunk(
   async (userInfo, thunkAPI) => {
     try {
       const response = await axios.post("/users/login", userInfo);
+      setAuthHeader(response.data.token);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -30,6 +46,7 @@ export const logIn = createAsyncThunk(
 export const logOut = createAsyncThunk("auth/logout", async (_, thunkAPI) => {
   try {
     const response = await axios.post("/users/logout");
+    clearAuthHeader();
     return response.data;
   } catch (error) {
     return thunkAPI.rejectWithValue(error.message);
